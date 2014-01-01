43415688c5ee7c6c866367ff711cd49489b4b53d
Freddie Rice <freddie.rice@gmail.com>
2014-01-01 12:58:19 -0600

    Made edits to the Makefile so it will only compile source files that need to be compiled (with compliance with the package name).  Configured Java Connection for Client object.

--- server/src/Client.java
+++ server/src/Client.java
@@ -8,13 +8,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Client{
+public class Client implements Runnable{
     public enum ClientStatus { NOT_AUTHORIZED, AUTHORIZED, DEAD}
 
     private int sessionID;
+    Connection conn = null;
     ClientStatus cs;
     Socket sock;
-    
+     
     Client()
     {
         
@@ -31,6 +32,29 @@ public class Client{
             return true;
         else
             return false;
+    }
+    
+    public void dbLogin()
+    {
+        try{
+            System.out.println("Connecting to database... ");
+            String url = "jdbc:mysql://127.0.0.1:3306/onebrown";
+            String user = "root";
+            //String pass = "R3xCtcDbU2mgG84";
+            String pass = "";
+            conn = DriverManager.getConnection(url,user,pass);
+        } catch(SQLException ex) {
+            System.out.println("error.");
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        System.out.println("done!");
+    }
+
+    public void run()
+    {
+        dbLogin();
     }
 
     public void authorize()
--- server/src/Main.java
+++ server/src/Main.java
@@ -1,5 +1,6 @@
 package pack;
 
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
