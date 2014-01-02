8ccc71055f4e09c6f6a9ecc7971bfac08d91e99b
Freddie Rice <freddie.rice@gmail.com>
2014-01-01 23:46:46 -0600

    Edited the Makefile to work with the classpath.  Added JSON support to the project.  Tested client <-> server interaction.

--- server/src/Client.java
+++ server/src/Client.java
@@ -8,13 +8,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+@SuppressWarnings("unchecked")
 public class Client implements Runnable{
     public enum ClientStatus { NOT_AUTHORIZED, AUTHORIZED, DEAD}
 
     private int sessionID;
     Connection conn = null;
     ClientStatus cs;
-    Socket sock;
+    Socket sock = null;
+    PrintWriter out = null;
+    BufferedReader in = null;
      
     Client()
     {
@@ -37,10 +43,9 @@ public class Client implements Runnable{
     public void dbLogin()
     {
         try{
-            System.out.println("Connecting to database... ");
+            System.out.print("Connecting to database... ");
             String url = "jdbc:mysql://127.0.0.1:3306/onebrown";
             String user = "root";
-            //String pass = "R3xCtcDbU2mgG84";
             String pass = "";
             conn = DriverManager.getConnection(url,user,pass);
         } catch(SQLException ex) {
@@ -49,16 +54,74 @@ public class Client implements Runnable{
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
         }
+        cs = ClientStatus.DEAD;
         System.out.println("done!");
     }
 
     public void run()
     {
         dbLogin();
+        initializeSocket();
+        authorize();
+    }
+
+    public void initializeSocket()
+    {
+        try {
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            out = new PrintWriter( sock.getOutputStream(), true);
+        }catch( IOException e ){}
     }
 
     public void authorize()
     {
+        JSONObject json = new JSONObject();
+        String username = "";
+        String password = "";
+
+        //tell the user to login
+        json.put("message","login");
+        sendJSONObject(json);
         
+        //get username and password
+        while(true){
+            json = (JSONObject)getJSONObject();
+            username = (String)json.get("username");
+            password = (String)json.get("password");
+
+            System.out.println("Username: " + username);
+            System.out.println("Password: " + password);
+        }
+    }
+
+    public void sendJSONObject(JSONObject json)
+    {
+        try{
+            StringWriter sw = new StringWriter();
+            String str = null;
+
+            json.writeJSONString(sw);
+            str = sw.toString();
+
+            out.println(str);
+        } catch( IOException e ){}
+    }
+
+    public Object getJSONObject()
+    {
+        Object obj  = null;
+        String str = "";
+        JSONParser parser = new JSONParser();
+        int length;
+        
+        try{
+            str = in.readLine();
+        } catch( IOException e ){}
+        
+        try{
+            obj = parser.parse(str);
+        } catch( ParseException e ){}
+
+        return obj;
     }
 }
